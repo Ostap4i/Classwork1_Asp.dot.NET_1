7ad2a504bb8229574b6fefb03f38be65865e5818
Ostap4ik777 <137522973+Ostap4i@users.noreply.github.com>
2024-05-17 18:14:58 +0300

    Add files via upload

---
+++ asp.dot.net/Service/AcauntService.cs
@@ -0,0 +1,20 @@
+﻿using asp.dot.net.Repositoriy;
+using asp.dot.net.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asp.dot.net.Service
+{
+    class AcauntService
+    {
+        private AcauntRepositoriy acauntRepositoriy = new AcauntRepositoriy();
+        public List<Acaunt> getAllAcaunts()
+        {
+            return acauntRepositoriy.getAllAcaunts();
+        }
+
+    }
+}
